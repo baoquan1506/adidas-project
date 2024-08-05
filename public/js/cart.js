21ad362348c40d646f26a4cf05ed597f08773eb5
baoquan1506 <baoquanwork@gmail.com>
2024-08-06 00:46:49 +0700

    success

---
+++ public/js/cart.js
@@ -0,0 +1,20 @@
+
+
+const payBtn = document.querySelector('.btn-buy');
+
+payBtn.addEventListener("click", () => {
+    fetch("/stripe-checkout", {
+        method: "post",
+        headers: new Headers({"Content-Type": "application/Json"}),
+        body: JSON.stringify({
+            items: JSON.parse(localStorage.getItem("cartItems")),
+        }),
+    })
+    .then((res) => res.json())
+    .then((url) => {
+        window.location.href = url;
+        clearCart();
+    })
+    .catch((err) => console.log(err));
+});
+
